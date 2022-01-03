d2b456ee0968512d6363abe1c56875484d413a3e
Juan Pablo Garcia Carrillo <juapgarc@gmail.com>
2022-01-03 09:55:42 +0100

    Updated package versions, please npm install!

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,7 @@
 import { send } from '@sapphire/plugin-editable-commands';
-import { Message, MessageEmbed } from 'discord.js';
+import { Message, MessageEmbed, TextChannel } from 'discord.js';
 import { RandomLoadingMessage } from './constants';
+import winston from 'winston';
 
 /**
  * Picks a random item from an array
@@ -26,4 +27,34 @@ export function sendLoadingMessage(message: Message): Promise<typeof message> {
  */
 export function randomColor() {
     return Math.floor(Math.random()*16777215).toString(16);
+}
+
+/**
+ * Will send a message to a text channel and ping the user, can be deleted after a timeout.
+ * @param channel - the channel to send the message to
+ * @param userId - the user to tag on the message
+ * @param message - the message to send
+ * @param timeout - timeout before delete if any, in seconds
+ * @async
+ */
+ export async function sendMsgToChannel(channel: TextChannel, userId: string, message: string, timeout: number = 0) {
+    winston.loggers.get(channel.guild.id).verbose(`A message has been sent to the channel ${channel.name} for the user with id ${userId} ${timeout === 0 ? 'with no timeout requested' : 'with a ' + timeout + ' second timeout.'}`);
+	
+	const message_1 = await channel.send('<@' + userId + '> ' + message);
+	await new Promise(() => setTimeout(() => message_1.delete(), timeout * 1000));
+
+	return message_1;
+
+    // if (timeout) msg.delete({timeout: timeout * 1000}); // convert to milliseconds
+}
+
+/**
+ * will shuffle an array as best and fast as possible
+ * @param array - array to shuffle
+ */
+ export function shuffleArray<T>(array: Array<T>) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
 }
--- src/objects/commons.ts
+++ src/objects/commons.ts
@@ -1,4 +1,4 @@
-import { PermissionOverwriteOptions, Snowflake } from "discord.js";
+import type { EmojiIdentifierResolvable, GuildMember, MessageReaction, PermissionOverwriteOptions, Snowflake, User } from "discord.js";
 
 /**
  * An object with a role and its permissions.
@@ -6,4 +6,23 @@ import { PermissionOverwriteOptions, Snowflake } from "discord.js";
  export interface RolePermission {
     id: Snowflake;
     permissions: PermissionOverwriteOptions;
+}
+
+/**
+ * The function to be called when a feature is activated.
+ */
+ export type FeatureCallback = (user: User, reaction: MessageReaction,
+    stopInteracting: Function, console: any) => Promise<void>;
+
+export type StopInteractingCallback = (user: User, reaction: MessageReaction,
+    stopInteracting: Function, console: any) => void;
+
+export type ShuffleFilter = (member: GuildMember) => boolean;
+
+export interface FeatureData {
+    name: string;
+    emojiResolvable: EmojiIdentifierResolvable;
+    description: string;
+    callback: FeatureCallback;
+    removeCallback?: StopInteractingCallback | undefined;
 }
